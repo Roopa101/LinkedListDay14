629755d25a585372f5bcdfc37b43e07ddd885af0
Roopa101 <83457806+Roopa101@users.noreply.github.com>
2021-12-09 19:24:48 +0530

    [Roopa] I created LinkedList Appending 30 and 56 to 70

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -10,18 +10,13 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            LinkedList list1 = new LinkedList();
-            list1.Add(56);
-            list1.Add(30);
-            list1.Add(70);
-            list1.Display();
-            LinkedList list2 = new LinkedList();
+            Console.WriteLine("Welcome to LinkedList Program");
+            LinkedList list = new LinkedList();
             //Appends the Nodes
-            list2.AddAppend(56);
-            list2.AddAppend(30);
-            list2.AddAppend(70);
-            list2.Display();
+            list.AddAppend(56);
+            list.AddAppend(30);
+            list.AddAppend(70);
+            list.Display();
             Console.ReadKey();
         }
     }
